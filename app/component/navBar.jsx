48925125a61984f4a2a9ea29eeab06507e4411a6
unknown <saad123mn123@gmail.com>
2023-10-12 18:36:41 +0300

    100

--- app/component/navBar.jsx
+++ app/component/navBar.jsx
@@ -38,20 +38,7 @@ export default function Nav() {
                 )}
               </Disclosure.Button>
             </div>
-            <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-12 sm:pr-0">
-              <Link
-                href={'/cart'}
-                type="button"
-                className="relative rounded-full p-1 text-gray-800 "
-              >
-                <span className="absolute -inset-1.5" />
-                <span className="sr-only">View Products</span>
-                <span className={`${numOfItems ? "absolute top-0 right-0 rounded-full main-bg text-white flex items-center justify-center w-4 h-4":"hidden"}`}>{numOfItems}</span>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
-                </svg>
-              </Link>
-            </div>
+          
             <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-between">
               <div className="flex flex-shrink-0 items-center">
                 <img
@@ -78,7 +65,20 @@ export default function Nav() {
                 </div>
               </div>
             </div>
-          
+            <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-12 sm:pr-0">
+              <Link
+                href={'/cart'}
+                type="button"
+                className="relative rounded-full p-1 text-gray-800 "
+              >
+                <span className="absolute -inset-1.5" />
+                <span className="sr-only">View Products</span>
+                <span className={`${numOfItems ? "absolute top-0 right-0 rounded-full main-bg text-white flex items-center justify-center w-4 h-4":"hidden"}`}>{numOfItems}</span>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
+                </svg>
+              </Link>
+            </div>
           </div>
         </div>
         <Disclosure.Panel className="sm:hidden">
