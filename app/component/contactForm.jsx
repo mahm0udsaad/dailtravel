0d773a151e328bf8ea36468ed513bc5559ea71e2
unknown <saad123mn123@gmail.com>
2023-10-12 21:47:09 +0300

    contact

---
+++ app/component/contactForm.jsx
@@ -0,0 +1,133 @@
+"use client"
+import React, { useState } from 'react';
+
+const ContactForm = () => {
+  const [formData, setFormData] = useState({
+    mobile: '',
+    city: 'الرياض',
+    destination: '',
+    hasFlightBooking: 'لا',
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  return (
+    <div className='py-8'>
+        <h1 className="text-center text-xl sm:text-3xl">
+        للطلب والاستفسار، يرجى تزويدنا ببياناتك وسنقوم بالتواصل معك في أسرع وقت ممكن
+
+        </h1>
+      <form>
+        <div className="mb-4">
+          <label className="block text-lg mb-2" htmlFor="mobile">
+            :الجوال
+          </label>
+          <input
+            type="text"
+            id="mobile"
+            name="mobile"
+            value={formData.mobile}
+            onChange={handleInputChange}
+            placeholder="+9xxxx"
+            className="border p-2 w-full"
+          />
+        </div>
+
+        <div className="mb-4">
+        <label className="block text-lg mb-2" htmlFor="city">
+            :مدينتك
+        </label>
+        <select
+            id="city"
+            name="city"
+            value={formData.city}
+            onChange={handleInputChange}
+            className="border p-2 w-full"
+        >
+            <option value="الرياض">الرياض</option>
+            <option value="جده">جده</option>
+            <option value="الشرقيه">الشرقيه</option>
+            <option value="اخري">اخري</option>
+        </select>
+        </div>
+
+
+        <div className="mb-4">
+          <label className="block text-lg mb-2" htmlFor="destination">
+            وين حاب تسافر:
+          </label>
+          <select
+            id="destination"
+            name="destination"
+            value={formData.destination}
+            onChange={handleInputChange}
+            className="border p-2 w-full"
+          >
+            <option value="">---</option>
+            <option value="جورجيا">جورجيا</option>
+            <option value="المالديف">المالديف</option>
+            <option value="ماليزيا">ماليزيا</option>
+            <option value="سريلانكا">سريلانكا</option>
+            <option value="تايلاند">تايلاند</option>
+            <option value="اندونيسيا">اندونيسيا</option>
+            <option value="تركيا">تركيا</option>
+            <option value="شرم الشيخ">شرم الشيخ</option>
+            <option value="رحلات كروز بحرية">رحلات كروز بحرية</option>
+            <option value="دول منطقه شنغن">دول منطقه شنغن</option>
+            <option value="موريشيوس">موريشيوس</option>
+            <option value="اذريبيجان">اذريبيجان</option>
+            <option value="البوسنه والهرسك">البوسنه والهرسك</option>
+            <option value="الامارات العربيه المتحده">الامارات العربيه المتحده</option>
+            <option value="البانيا">البانيا</option>
+            <option value="لندن">لندن</option>
+            <option value="مصر">مصر</option>
+          </select>
+        </div>
+
+        <div className="mb-4">
+          <label className="block text-lg mb-2">هل تم حجز الطيران؟</label>
+          <div className="flex items-center justify-end">
+            <input
+              type="radio"
+              id="hasFlightYes"
+              name="hasFlightBooking"
+              value="نعم"
+              checked={formData.hasFlightBooking === 'نعم'}
+              onChange={handleInputChange}
+              className="mr-2 w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 focus:ring-2"
+            />
+            <label className="mr-4" htmlFor="hasFlightYes">
+              نعم
+            </label>
+
+            <input
+              type="radio"
+              id="hasFlightNo"
+              name="hasFlightBooking"
+              value="لا"
+              checked={formData.hasFlightBooking === 'لا'}
+              onChange={handleInputChange}
+              className="mr-2 w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 focus:ring-2"
+            />
+            <label htmlFor="hasFlightNo">لا</label>
+          </div>
+        </div>
+
+        <button
+          type="submit"
+          className="bg-blue-500 text-white py-2 px-4 rounded"
+        >
+          إرسال
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default ContactForm;
--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,8 +1,34 @@
 
+"use client"
+import { FaLocationDot } from 'react-icons/fa';
+import {BiSupport , BiLocationPlus} from 'react-icons/bi'
+import ContactForm from '../component/contactForm';
 
 const Contact= () =>{
     return(
-        <h1>Hello</h1>
+       <main className='pt-20 text-end px-8'>
+        <ContactForm/>
+            <div className="border-b border-gray-300 pb-4">
+            <h2 className="text-xl font-semibold">كيف تصل الينا</h2>
+            <div className="flex items-center justify-end mt-2 w-full">
+            <div>
+                <p>:الفرع الرئيسي</p>
+                <div className='flex'>
+                <p>العنوان: الخبر - شارع عبدالرحمن بن معاذ</p>
+                <div className="w-12 flex  justify-center">
+            <BiLocationPlus className="w-12 flex text-gray-500" />
+                </div>
+                </div>
+                <div className='flex'>
+                <p>sales@dalitravelsa.com:الدعم الفني</p>
+                <div className="w-12 flex  justify-center">
+            <BiSupport className=" text-gray-500" />
+                </div>
+                </div>
+            </div>
+            </div>
+        </div>
+       </main>
     )
 }
 export default Contact;
